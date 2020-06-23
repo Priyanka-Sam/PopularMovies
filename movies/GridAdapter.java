0f7a15f177b1bca053f327138bfd365a1cbd91e8
Priyanka-Sam <46075615+Priyanka-Sam@users.noreply.github.com>
2020-06-23 19:03:02 +0530

    Add files via upload

---
+++ movies/GridAdapter.java
@@ -0,0 +1,98 @@
+package com.example.movies;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.MoviesGridViewHolder> {
+
+
+    private static final String POSTER_PATH = "poster_path";
+    private ArrayList<HashMap<String, String>> moviesList = new ArrayList<>();
+    private LayoutInflater mInflater;
+    private ArrayList<String> moviePosters = new ArrayList<>();
+    private Context mContext;
+
+    private final GridAdapterOnClickHandler mClickHandler;
+
+    public interface GridAdapterOnClickHandler {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+
+    public GridAdapter(Context context, GridAdapterOnClickHandler listener) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mClickHandler = listener;
+    }
+
+
+
+    public void setMovieData(ArrayList<HashMap<String,String>> data){
+        moviesList = data;
+        if(moviesList != null) {
+            moviePosters.clear();
+            for (HashMap<String, String> movie : moviesList) {
+                moviePosters.add(movie.get(POSTER_PATH));
+            }
+        }
+        else if (moviesList == null){
+            moviePosters = null;
+        }
+        notifyDataSetChanged();
+    }
+
+
+    public class MoviesGridViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView mMoviePoster;
+        public MoviesGridViewHolder(View item){
+            super(item);
+            mMoviePoster = (ImageView) item.findViewById(R.id.iv_movie_poster);
+            item.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedItem = getAdapterPosition();
+            mClickHandler.onListItemClick(clickedItem);
+        }
+    }
+
+
+    @Override
+    public MoviesGridViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.grid_item, parent, false);
+        return new MoviesGridViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MoviesGridViewHolder holder, int position) {
+        String moviePosterPath = moviePosters.get(position);
+        final String BASE_URL = "http://image.tmdb.org/t/p/";
+        final String IMAGE_SIZE = "w500";
+        Uri posterPath = Uri.parse(BASE_URL).buildUpon().appendPath(IMAGE_SIZE).appendEncodedPath(moviePosterPath).build();
+        Picasso.with(mContext).load(posterPath.toString()).into(holder.mMoviePoster);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(moviePosters!= null) {
+            return moviePosters.size();
+        }
+        else
+            return 0;
+    }
+
+}
